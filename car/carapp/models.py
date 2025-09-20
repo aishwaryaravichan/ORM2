b1ed64ede9b9a2579ade78874209b83814861bc8
aishwaryaravichan <aishuuravichandran@gmail.com>
2025-09-21 00:25:30 +0530

    success

---
+++ car/carapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib import admin
+class Car(models.Model):
+    car_name= models.CharField()
+    car_model = models.CharField()
+    release_date = models.DateField()
+    price = models.IntegerField()
+    color = models.CharField()
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('car_name', 'car_model', 'release_date', 'price', 'color')
+
+# Create your models here.
